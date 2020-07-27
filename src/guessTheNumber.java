01cc51b9a706390a706265ab356104e5e99c9678
Baki-code <t.a.spiridonob@gmail.com>
2020-07-27 15:23:00 +0300

    Initial commit

---
+++ src/guessTheNumber.java
@@ -0,0 +1,5 @@
+public class guessTheNumber {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/guessTheWord.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class guessTheWord {
+    static Scanner scanner = new Scanner(System.in);
+    static Random random = new Random();
+    
+    public static void main(String[] args) {
+
+    }
+}
